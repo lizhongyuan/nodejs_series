de1eab11820ec7571e58671fe05f52ec3085c6a9
lizhongyuan <hybridblader@163.com>
2016-02-04 00:01:47 +0800

    add testPromise.js in Hybridbalder, and technode

--- spider/test/testPromise.js
+++ spider/test/testPromise.js
@@ -1,118 +1,44 @@
-var rp = require('request-promise');
-//var rp = require('request');
+var fs = require('fs');
+var txt = "以上程序使用fs.readFileSync从源路径读取文件内容，并使用fs.writeFileSync将文件内容写入目标路径。";
 
-var cheerio = require('cheerio'); // Basically jQuery for node.js
+var Promise = require('bluebird');
 
+var pfs = Promise.promisifyAll(fs);
+
+var getFileAsync = function(fileName) {
+    return pfs.readFileAsync(fileName, "utf8").then(function(data){
+        return String(data);
+    });
+}
+
+var fileData = getFileAsync("message.txt");
+console.log(fileData);
+/*
+pfs.readFileAsync("message.txt", "utf8").then(function(data) {
+    console.log(data);
+});
+*/
+
+//var fsPromise = pfs.readFileAsync("message.txt", "utf8");
 
 /*
-var options = {
-    uri: 'http://www.google.com',
-    transform: function (body) {
-        return cheerio.load(body);
-    }
-};
-rp(options)
-    .then(function ($) {
-        // Process html like you would with jQuery...
-    })
-    .catch(function (err) {
-        // Crawling failed or Cheerio choked...
-    });
-    */
-var options = {
-    url: 'http://dota2.vpgame.com/guess/default-index.html?ajax=yw1&page=1',
-    transform: function(body){
-        return cheerio.load(body);
-    }
-}
-
-var totalOptions = [{
-    url: 'http://dota2.vpgame.com/guess/default-index.html?ajax=yw1&page=1',
-    transform: function (body) {
-        return cheerio.load(body);
-    }
-}, {
-    url: 'http://dota2.vpgame.com/guess/default-index.html?ajax=yw1&page=2',
-    transform: function(body){
-        return cheerio.load(body);
-    }
-}, {
-    url: 'http://dota2.vpgame.com/guess/default-index.html?ajax=yw1&page=3',
-    transform: function(body){
-        return cheerio.load(body);
-    }
-}
-];
-
-
-var getCurPageTask2 = function(options) {
-    rp(options).then(function ($) {
-        var spinachItems = $('div.spinach-item');
-        var curPageTasks = [];
-
-        spinachItems.each(function () {
-            var curItem = $(this);
-            var matchType = curItem.find('i').attr("class");
-            if (matchType != "dota2-icon") {
-                return;
-            }
-            var matchName = curItem.find('span.spinach-league').text();
-            var leftTeam = curItem.find('div.spinach-item-team.pull-left div.spinach-item-data span.ellipsis').text();
-            var rightTeam = curItem.find('div.spinach-item-team.pull-right div.spinach-item-data span.ellipsis').text();
-            var boNum = curItem.find('div.spinach-item div.spinach-item-vs span').text();
-
-            curPageTasks.splice(curPageTasks.length, 0, [leftTeam, rightTeam, boNum, matchName]);
-        });
-        return curPageTasks;
-    });
-}
-
-var getCurPageTask = function(options, callback) {
-    var curPageTasks = [];
-    rp(options).then(function ($) {
-        var spinachItems = $('div.spinach-item');
-
-        spinachItems.each(function () {
-            var curItem = $(this);
-            var matchType = curItem.find('i').attr("class");
-            if (matchType != "dota2-icon") {
-                return;
-            }
-            var matchName = curItem.find('span.spinach-league').text();
-            var leftTeam = curItem.find('div.spinach-item-team.pull-left div.spinach-item-data span.ellipsis').text();
-            var rightTeam = curItem.find('div.spinach-item-team.pull-right div.spinach-item-data span.ellipsis').text();
-            var boNum = curItem.find('div.spinach-item div.spinach-item-vs span').text();
-
-            curPageTasks.splice(curPageTasks.length, 0, [leftTeam, rightTeam, boNum, matchName]);
-        });
-        //callback(null, curPageTasks);
-        console.log(curPageTasks);
-        callback(null, curPageTasks);
-    });
-}
-
-var getTasks = function(totalOptions, totalTaskArray, callback) {
-    for(var i = 0; i < totalOptions.length; i++) {
-        getCurPageTask(totalOptions[i], function(err, curPageTasks){
-            totalTaskArray += curPageTasks;
-        });
-    }
-    //console.log(totalTaskArray);
-    callback(totalTaskArray);
-}
-
-getTasks(totalOptions, [], function(totalTaskArray){
-    console.log("Fuck");
-})
-
-/*
-getCurPageTask(options,function(err, curPageTasks){
-    console.log(curPageTasks);
+fsPromise.then(function(data){
+    console.log(data);
+}).catch(function(e){
+    console.error(e.stack);
 });
 */
 
 /*
-var resArray = getCurPageTask2(options);
-console.log(resArray);
-*/
+// 写入文件
+fs.writeFile('message.txt', txt, function (err) {
+    if (err) throw err;
+    console.log('It\'s saved!'); //文件被保存
+});
 
+// 读取文件
+fs.readFile('message.txt', 'utf8', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+});
+*/
--- spider/update/match_list.js
+++ spider/update/match_list.js
@@ -5,73 +5,122 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var debug = require('debug');
-var async = require('async');
+var Fiber = require('fibers');
 
-var promise = require("bluebird");
-//使用bluebird将request库的所有异步方法编译成promise形式，会自动加上Async后缀
-request = promise.promisifyAll(request);
 
-//var totalPageTasks = [];
+/*
+ * 从curTaskUrl中抓取比赛信息，回调函数callback(err, curPageTasks),
+ * 第二个参数为结果
+ */
+var getCurPageTask = function(curTaskUrl, callback) {
+    var curPageTasks = [];      //结果
+    Fiber(function() {
+        var curFiber = Fiber.current;       //拿到当前Fiber
 
-var getCurPageTask = function(curTaskUrl, curIndex, callback) {
-    console.log(curIndex);
-    var curPageTasks = [];
-    request(curTaskUrl, function (err, res) {
-        if (err) return callback(err);
-
-        var $ = cheerio.load(res.body.toString());
-        var spinachItems = $('div.spinach-item');
-
-        spinachItems.each(function () {
-            var curItem = $(this);
-            var matchType = curItem.find('i').attr("class");
-            if (matchType != "dota2-icon") {
-                return;
+        request(curTaskUrl, function (err, res) {
+            if (err) {
+                return callback(err);
             }
-            var matchName = curItem.find('span.spinach-league').text();
-            var leftTeam = curItem.find('div.spinach-item-team.pull-left div.spinach-item-data span.ellipsis').text();
-            var rightTeam = curItem.find('div.spinach-item-team.pull-right div.spinach-item-data span.ellipsis').text();
-            var boNum = curItem.find('div.spinach-item div.spinach-item-vs span').text();
 
-            curPageTasks.splice(curPageTasks.length, 0, [leftTeam, rightTeam, boNum, matchName]);
-        });
-        if(curIndex-- <= 0){
+            var $ = cheerio.load(res.body.toString());
+            var spinachItems = $('div.spinach-item');
+
+            spinachItems.each(function () {
+                var curItem = $(this);
+                var matchType = curItem.find('i').attr("class");
+                if (matchType != "dota2-icon") {
+                    return;
+                }
+                var matchName = curItem.find('span.spinach-league').text();
+                var leftTeam = curItem.find('div.spinach-item-team.pull-left div.spinach-item-data span.ellipsis').text();
+                var rightTeam = curItem.find('div.spinach-item-team.pull-right div.spinach-item-data span.ellipsis').text();
+                var boNum = curItem.find('div.spinach-item div.spinach-item-vs span').text();
+
+                curPageTasks.splice(curPageTasks.length, 0, [leftTeam, rightTeam, boNum, matchName]);
+            });
             callback(null, curPageTasks);
+            curFiber.run();
+        });     // request end;
+        Fiber.yield();
+        return curPageTasks;
+    }).run();
+}
+
+
+/*
+ *
+ */
+var getNumPageTasks = function(feedUrl, pageNum, totalPageTasks, callback) {
+    Fiber(function(){
+        var curFiber = Fiber.current;
+        for(var i = 1; i <= pageNum; i++){
+            var curPageUrl = feedUrl + String(i);
+            getCurPageTask(curPageUrl, function(err, array){
+                totalPageTasks = totalPageTasks.concat(array);
+                if(i == pageNum){
+                    callback(pageNum, i, totalPageTasks);
+                }
+                curFiber.run(); //解除阻塞
+            });
+            Fiber.yield();      // 阻塞
         }
-    });
+    }).run();
 }
 
+/*
+var getPageNum = function(url){
+    var pageNum;
+    Fiber(function(){
+        var curFiber = Fiber.current;
+        request(url, function(err, res){
+            if(err) {
+                throw DOMException;
+            }
 
-//var getTasks = function(pageNum, callback) {
-var getTasks = function(pageNum, totalPageTasks, callback) {
-    var preUrl = "http://dota2.vpgame.com/guess/default-index.html?ajax=yw1&page=";
-    var urlList = [];
-    for(var i = 1; i <= pageNum; i++){
-        /*
-        var curPageUrl = preUrl + String(i);
-        getCurPageTask(curPageUrl, function(err, array){
-            totalPageTasks += array;
-            callback(totalPageTasks);
+            var $ = cheerio.load(res.body.toString());
+            var spinachItems = $('div.pager li.page');
+            pageNum = spinachItems.length;
+            curFiber.run();
         });
-        */
-        var curPageUrl = preUrl + String(i);
-        urlList.splice(urlList.length, 0, curPageUrl);
-    }
-    //console.log(urlList);
-    //var totalPageTasks = [];
+        Fiber.yield();
+        console.log(pageNum);
+    }).run();
+}
+*/
+var getPageNum = function(url, callback){
+    var pageNum;
+    Fiber(function(){
+        var curFiber = Fiber.current;
+        request(url, function(err, res){
+            if(err) {
+                throw DOMException;
+            }
 
-    async.eachSeries(urlList, function(curPageUrl, next){
-        getCurPageTask(curPageUrl,pageNum, function(err, array){
-            totalPageTasks += array;
-            //console.log(totalPageTasks);
+            var $ = cheerio.load(res.body.toString());
+            var spinachItems = $('div.pager li.page');
+            pageNum = spinachItems.length;
+            callback("", pageNum);
+            curFiber.run();
         });
-        next();
-    }, function(err){
-        console.log("...");
-        callback(totalPageTasks);
-    });
+        Fiber.yield();
+    }).run();
 }
 
-getTasks(2, [], function(totalPageTasks){
-    //console.log(totalPageTasks)
+var getTasks = function() {
+    var feedUrl = "http://dota2.vpgame.com/guess/default-index.html?ajax=yw1&page=";
+    var pageNum = getPageNum("http://dota2.vpgame.com/guess.html");
+    console.log(pageNum);
+}
+
+getTasks();
+
+/*
+getNumPageTasks("http://dota2.vpgame.com/guess/default-index.html?ajax=yw1&page=", 2, [], function(pageNum, i, totalPageTasks){
+    console.log(totalPageTasks);
+    console.log(totalPageTasks.length);
 });
+*/
+var  res = getCurPageTask("http://dota2.vpgame.com/guess/default-index.html?ajax=yw1&page=", function(){
+
+});
+console.log(res);
