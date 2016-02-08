df5225c1ba41d3d9de4f03737750d52e0bed1893
lizhongyuan <hybridblader@163.com>
2016-02-08 17:30:02 +0800

    add my demo chat-example, can run easily

--- chat-example/index.js
+++ chat-example/index.js
@@ -1,5 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var express = require('express');
+var path = require('path');
 
 // 加载socket.io, 并且new一个instance
 var io = require('socket.io')(http);
@@ -9,6 +11,8 @@ app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
 */
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // 改为发送一个html file
 app.get('/', function(req, res){
@@ -20,10 +24,26 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 */
+/*
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
+  });
+});
+*/
+/*
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+*/
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('chat message: ' + msg);
+    io.emit('chat message', msg);
   });
 });
 
