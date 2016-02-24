f581a5124d2a4fe576b7e5bdad5d14ee3976f7ac
lizhongyuan <hybridblader@163.com>
2016-02-25 01:50:30 +0800

    add the sample_chat2

--- sample_chat2/socket/io.js
+++ sample_chat2/socket/io.js
@@ -1,17 +1,22 @@
 /**
  * Created by svenlee on 16/2/12.
  */
+module.exports = function(io) {
+    var messages = []
 
-var messages = [];
-
-io.sockets.on("connection", function(socket){
-    socket.on("getAllMessages", function(){
-        // 将所有消息推送到客户端
-        socket.emit("allMessages", messages);
-    });
-
-    socket.on("createMessage", function(message){
-        messages.push(message);
-        socket.emit("messageAdded", message);
-    });
-});
+    io.sockets.on('connection', function (socket) {
+        socket.on('getAllMessages', function () {
+            console.log("Server receive getAllMessages.");
+            console.log("Server emit allMessages.");
+            socket.emit('allMessages', messages)
+        })
+        socket.on('createMessage', function (message) {
+            //
+            messages.push(message);
+            console.log("Server receive createMessage.");
+            console.log("Server emit messageAdded.");
+            //io.sockets.emit('messageAdded', message)
+            socket.emit('messageAdded', message)
+        })
+    })
+}
--- sample_chat2/static/technode.js
+++ sample_chat2/static/technode.js
@@ -1,22 +1,20 @@
-/**
- * Created by svenlee on 16/2/13.
- */
+angular.module('techNodeApp', [])
 
-// 声明一个名为technodeApp的模块
-angular.module('techNodeApp', []);
-
-angular.module('techNodeApp').factory('socket', function($rootScope) {
+//socket.on()
+angular.module('techNodeApp').factory('socketService', ["$rootScope", function($rootScope) {
     // var socket = io.connect('/')
     //为了避免跨域请求，需要将原来的var socket = io.connect('/')改成下面这一行
     // var socket = io.connect('http://localhost:3000/')
     //或者采用socket官方主页上面的方法 var socket = io()
+    //var socket = io.connect('/');
     var socket = io();
     return {
         on: function(eventName, callback) {
             socket.on(eventName, function() {
-                var args = arguments
+                var args = arguments            //
                 $rootScope.$apply(function() {
                     callback.apply(socket, args)
+                    //callback.apply(socket, arguments)
                 })
             })
         },
@@ -31,4 +29,76 @@ angular.module('techNodeApp').factory('socket', function($rootScope) {
             })
         }
     }
+}])
+
+
+/*
+ * textarea ctrl-enter-break-line="createMessage()"
+ */
+angular.module('techNodeApp').directive('ctrlEnterBreakLine', function() {
+    return function(scope, element, attrs) {        // element 就是指当前的tag
+        var ctrlDown = false
+        // bind: sync data with
+        element.bind("keydown", function(evt) {
+            if (evt.which === 17) {
+                ctrlDown = true
+                setTimeout(function() {
+                    ctrlDown = false
+                }, 1000)
+            }
+            if (evt.which === 13) {
+                if (ctrlDown) {
+                    element.val(element.val() + '\n')
+                } else {
+                    scope.$apply(function() {
+                        scope.$eval(attrs.ctrlEnterBreakLine);
+                    });
+                    evt.preventDefault()
+                }
+            }
+        });
+    };
 });
+
+//angular.module('techNodeApp').controller('MessageCreatorCtrl', function($scope, socket) {
+angular.module('techNodeApp').controller('MessageCreatorCtrl', ["$scope", "socketService", function($scope, socketService) {
+    $scope.newMessage = ''
+    $scope.createMessage = function () {
+        if ($scope.newMessage == '') {
+            return
+        }
+        socketService.emit('createMessage', $scope.newMessage)
+        $scope.newMessage = ''
+    }
+}])
+
+angular.module('techNodeApp').directive('autoScrollToBottom', function() {
+    return {
+        link: function(scope, element, attrs) {
+            scope.$watch(
+                function() {
+                    return element.children().length;
+                },
+                function() {
+                    element.animate({
+                        scrollTop: element.prop('scrollHeight')
+                    }, 1000);
+                }
+            );
+        }
+    };
+});
+
+//angular.module('techNodeApp').controller('RoomCtrl', function($scope, socket) {
+angular.module('techNodeApp').controller('RoomCtrl', ["$scope", "socketService", function($scope, socket) {
+    $scope.messages = []
+    console.log("try to getAllMessages.");
+    socket.emit('getAllMessages')
+    socket.on('allMessage', function (messages) {
+        console.log("Client get all messages.");
+        $scope.messages = messages
+    })
+    socket.on('messageAdded', function (message) {
+        $scope.messages.push(message)
+    })
+}])
